19d1d573eeee128941f08f9bf641f406e57f5976
ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-06-10 14:55:15 +0700

    move some code from App.php to Common.php

--- app/Common.php
+++ app/Common.php
@@ -1,5 +1,16 @@
 <?php
 
+date_default_timezone_set('Asia/Jakarta');
+define('DATE_NOW', date('Y-m-d H:i:s', time()));
+
+$base_url_ = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$base_url_ .= "://" . $_SERVER['HTTP_HOST'];
+$base_url_ .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+define('BASE_URL', $base_url_);
+define('ADMIN_PATH', '/ruangadmin');
+define('API_PATH', '/api');
+define('SALT', 'hehehehe');
+
 /**
  * The goal of this file is to allow developers a location
  * where they can overwrite core procedural functions and
