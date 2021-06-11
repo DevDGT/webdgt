a94f60ba3ac1a15abcb5cd5eb599ada5ba4b3734
ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-06-11 15:58:41 +0700

    remove comment

--- app/Views/admin/layouts/nav.php
+++ app/Views/admin/layouts/nav.php
@@ -56,38 +56,9 @@
 				</li>
 			</ul>
 		</li>
-		<!-- <li class="nav-item">
-			<a href="#" class="nav-link">
-				<i class="nav-icon fas fa-cogs"></i>
-				<p>
-					Pengaturan
-					<i class="right fas fa-angle-left"></i>
-				</p>
-			</a>
-			<ul class="nav nav-treeview">
-				<li class="nav-item">
-					<a href="./index.html" class="nav-link">
-						<i class="far fa-circle nav-icon"></i>
-						<p>Dashboard v1</p>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a href="./index2.html" class="nav-link">
-						<i class="far fa-circle nav-icon"></i>
-						<p>Dashboard v2</p>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a href="./index3.html" class="nav-link">
-						<i class="far fa-circle nav-icon"></i>
-						<p>Dashboard v3</p>
-					</a>
-				</li>
-			</ul>
-		</li> -->
 		<li class="nav-header">Profile</li>
 		<li class="nav-item">
-			<a href="<?= base_url(ADMIN_PATH . '/user/settings') ?>" class="nav-link menu-item <?= ($menu ?? "") == 'userSettings' ? 'active' : '' ?>">
+			<a href="<?= base_url(ADMIN_PATH . '/profile') ?>" class="nav-link menu-item <?= ($menu ?? "") == 'profile' ? 'active' : '' ?>">
 				<i class="nav-icon fas fa-user-cog"></i>
 				<p>
 					Pengaturan Profile
