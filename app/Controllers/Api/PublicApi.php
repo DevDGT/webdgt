5be08794c25a65134aebfecd7f71fb548521ae4f
Kadal <riz.idn.dev@gmail.com>
2021-07-06 14:01:46 +0700

    on the way category

--- app/Controllers/Api/PublicApi.php
+++ app/Controllers/Api/PublicApi.php
@@ -6,7 +6,7 @@ use App\Controllers\BaseController;
 
 class PublicApi extends BaseController
 {
-    function __construct()
+    public function __construct()
     {
         // $this->masterModel = new \App\Models\MasterModel();
         $this->db = \Config\Database::connect();
@@ -15,7 +15,6 @@ class PublicApi extends BaseController
     public function getArticle()
     {
         try {
-
             $limit = getNewsParam('limit', 0);
             $page = getNewsParam('page', 0);
             $idArticle = getNewsParam('id');
@@ -45,14 +44,22 @@ class PublicApi extends BaseController
             $this->builder->join('category c', 'c.id = a.category_id');
             $this->builder->join('users up', 'up.id = a.update_by');
             $this->builder->orderBy('id', 'desc');
-            if ($idArticle != '') $this->builder->where(EncKey('a.id'), $idArticle);
-            if ($slugArticle != '') $this->builder->where('a.slug', $slugArticle);
-            if ($slugCategory != '') $this->builder->where('c.slug', $slugCategory);
-            if ($tag != '') $this->builder->like('a.tags', $tag, 'both');
+            if ($idArticle != '') {
+                $this->builder->where(EncKey('a.id'), $idArticle);
+            }
+            if ($slugArticle != '') {
+                $this->builder->where('a.slug', $slugArticle);
+            }
+            if ($slugCategory != '') {
+                $this->builder->where('c.slug', $slugCategory);
+            }
+            if ($tag != '') {
+                $this->builder->like('a.tags', $tag, 'both');
+            }
             $this->builder->where(['status' => '1']);
             $this->builder->limit($limit, ($page != 0 ? $page - 1 : 0) * $limit);
             $article = $this->builder->get()->getResultArray();
-            $data = array();
+            $data = [];
             $field = [
                 'title',
                 'slug',
@@ -68,30 +75,32 @@ class PublicApi extends BaseController
                 'updated_at',
             ];
             foreach ($article as $field_) {
-                $row = array();
+                $row = [];
                 $row['id'] = Enc($field_['id']);
                 foreach ($field as $key) {
                     $row[$key] = $field_[$key];
                 }
-                $row['description'] = trim(preg_replace('!\s+!', ' ', (substr(strip_tags($field_['content']), 0, 400) . "...")));
-                if ($detail == 'true') $row['content'] = $field_['content'];
+                $row['description'] = trim(preg_replace('!\s+!', ' ', (substr(strip_tags($field_['content']), 0, 400).'...')));
+                if ($detail == 'true') {
+                    $row['content'] = $field_['content'];
+                }
                 $data[] = $row;
             }
 
             $result = [
                 'status' => 'ok',
                 'count' => count($article),
-                'data' => $data
+                'data' => $data,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage() . " " . $th->getFile() . " Line : " . $th->getLine()
+                'message' => $th->getMessage().' '.$th->getFile().' Line : '.$th->getLine(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -101,22 +110,21 @@ class PublicApi extends BaseController
     public function getCategory()
     {
         try {
-
             $category = $this->db->query("SELECT `cat`.`name`, `cat`.`slug`, (SELECT COUNT(*) FROM article WHERE category_id = cat.id AND status = '1') count FROM `category` `cat` WHERE (SELECT COUNT(*) FROM article WHERE category_id = cat.id AND status = '1') != '0' ORDER BY `count` DESC")->getResult();
 
             $result = [
                 'status' => 'ok',
-                'data' => $category
+                'data' => $category,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage() . $this->db->getLastQuery()
+                'message' => $th->getMessage().$this->db->getLastQuery(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -127,32 +135,34 @@ class PublicApi extends BaseController
     {
         try {
             $this->builder = $this->db->table('article a');
-            $this->builder->select("
+            $this->builder->select('
                 a.tags,
-            ");
-            $this->builder->where("status", '1');
+            ');
+            $this->builder->where('status', '1');
             $tags = $this->builder->get()->getResult();
             $tagsData = [];
             foreach ($tags as $field_) {
-                $listTag = explode(" ", $field_->tags);
+                $listTag = explode(' ', $field_->tags);
                 foreach ($listTag as $tag) {
-                    if (!in_array($tag, $tagsData)) array_push($tagsData, $tag);
+                    if (!in_array($tag, $tagsData)) {
+                        array_push($tagsData, $tag);
+                    }
                 }
             }
             $result = [
                 'status' => 'ok',
                 'count' => count($tagsData),
-                'data' => $tagsData
+                'data' => $tagsData,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage()
+                'message' => $th->getMessage(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -167,7 +177,7 @@ class PublicApi extends BaseController
     private function getClientProducts($productId)
     {
         return $this->db->table('clients_orders co')
-            ->select(EncKey('c.id') . ' id, c.name')
+            ->select(EncKey('c.id').' id, c.name')
             ->join('clients c', 'c.id = co.id_clients')
             ->where('co.id_products', $productId)
             ->get()->getResultArray();
@@ -177,15 +187,15 @@ class PublicApi extends BaseController
     {
         try {
             $this->builder = $this->db->table('teams t');
-            $this->builder->select("
+            $this->builder->select('
                 u.id,
                 u.name,
                 u.quotes,
                 u.photo,
                 j.name jobs
-            ");
-            $this->builder->join("users u", "u.id = t.user_id");
-            $this->builder->join("jobs j", "j.id = t.job_id");
+            ');
+            $this->builder->join('users u', 'u.id = t.user_id');
+            $this->builder->join('jobs j', 'j.id = t.job_id');
             $this->builder->orderby('j.order', 'asc');
             $this->builder->orderby('u.name', 'asc');
             $this->builder->where('u.active', '1');
@@ -193,7 +203,7 @@ class PublicApi extends BaseController
             $field = ['name', 'quotes', 'photo', 'jobs'];
             $data = [];
             foreach ($teams as $field_) {
-                $row = array();
+                $row = [];
                 foreach ($field as $key) {
                     $row[$key] = $field_[$key];
                 }
@@ -203,17 +213,17 @@ class PublicApi extends BaseController
             $result = [
                 'status' => 'ok',
                 'count' => count($data),
-                'data' => $data
+                'data' => $data,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage()
+                'message' => $th->getMessage(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -223,25 +233,24 @@ class PublicApi extends BaseController
     public function getClients()
     {
         try {
-
             $clients = $this->db->table('clients')
-                ->select(EncKey('id') . 'id ,name, icon, description')
+                ->select(EncKey('id').'id ,name, icon, description')
                 ->where('active', '1')
                 ->orderby('id', 'desc')->get()->getResult();
             $result = [
                 'status' => 'ok',
                 'count' => count($clients),
-                'data' => $clients
+                'data' => $clients,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage()
+                'message' => $th->getMessage(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -251,9 +260,8 @@ class PublicApi extends BaseController
     public function getClientsOrders($idClient)
     {
         try {
-
             $clients = $this->db->table('clients_orders co')
-                ->select(EncKey('co.id') . 'id, p.name, p.icon, p.description')
+                ->select(EncKey('co.id').'id, p.name, p.icon, p.description')
                 ->where('co.active', '1')
                 ->where(EncKey('co.id_clients'), $idClient)
                 ->join('products p', 'p.id = co.id_products')
@@ -261,17 +269,17 @@ class PublicApi extends BaseController
             $result = [
                 'status' => 'ok',
                 'count' => count($clients),
-                'data' => $clients
+                'data' => $clients,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage()
+                'message' => $th->getMessage(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -281,19 +289,18 @@ class PublicApi extends BaseController
     public function getProducts($idProducts = '')
     {
         try {
-
             $this->builder = $this->db->table('products');
             $this->builder->select('id ,name, icon, video, description');
             $this->builder->where('active', '1');
-            if ($idProducts != '') $this->builder->where(EncKey('id'), $idProducts);
+            if ($idProducts != '') {
+                $this->builder->where(EncKey('id'), $idProducts);
+            }
             $clients = $this->builder->orderby('id', 'desc')->get()->getResultArray();
-
-
 
             $field = ['name', 'icon', 'video', 'description'];
             $data = [];
             foreach ($clients as $field_) {
-                $row = array();
+                $row = [];
                 $row['id'] = Enc($field_['id']);
                 foreach ($field as $key) {
                     $row[$key] = $field_[$key];
@@ -304,17 +311,17 @@ class PublicApi extends BaseController
             $result = [
                 'status' => 'ok',
                 'count' => count($clients),
-                'data' => $data
+                'data' => $data,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage()
+                'message' => $th->getMessage(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
@@ -325,24 +332,24 @@ class PublicApi extends BaseController
     {
         try {
             $clients = $this->db->table('products_demo')
-                ->select(EncKey('id') . 'id ,title, link')
+                ->select(EncKey('id').'id ,title, link')
                 ->where('active', '1')
                 ->where(EncKey('product_id'), $idProducts)
                 ->orderby('id', 'desc')->get()->getResult();
             $result = [
                 'status' => 'ok',
                 'count' => count($clients),
-                'data' => $clients
+                'data' => $clients,
             ];
         } catch (\Throwable $th) {
             $result = [
                 'status' => 'fail',
-                'message' => $th->getMessage()
+                'message' => $th->getMessage(),
             ];
         } catch (\Exception $ex) {
             $result = [
                 'status' => 'fail',
-                'message' => $ex->getMessage()
+                'message' => $ex->getMessage(),
             ];
         } finally {
             echo json_encode($result);
--- public/assets/js/page/product.js
+++ public/assets/js/page/product.js
@@ -2,14 +2,15 @@ var portfolioIsotope
 
 $(document).ready(function () {
   // alert("asddsa")
-  usersProduct();
-  initPortpolio();
+  // initPortpolio();
+  getProduct();
+  // initFetch();
 });
 
 function usersProduct() {
   $('.userProduct').slick({
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 3,
     dots: true,
     autoplay: true,
@@ -18,6 +19,32 @@ function usersProduct() {
     autoplaySpeed: 3000,
     speed: 1000,
     centerMode: false,
+  });
+}
+
+function getClients() {
+  console.log('clientGet');
+  // alert('clientGet')
+  return new Promise(resolve => {
+    var clientsAPI = `${API_PATH}/public/get/clients`;
+    $.getJSON(clientsAPI, {
+      format: 'json'
+    }).done(function (response) {
+      let clients = '';
+      $.each(response.data, function (i, items) {
+        clients += `
+                  <div class="col-lg-3 col-md-4 col-6">
+                      <div class="client-logo">
+                          <img src="${BASE_URL}/uploads/clients/${items.icon}" class="img-fluid" alt="${items.name}" title="${items.description}">
+                      </div>
+                  </div>
+                  `;
+        $('#clientsData').html(clients);
+        // $('#clientsData').removeClass('d-none');
+        resolve(true);
+      });
+    });
+
   });
 }
 
@@ -47,7 +74,43 @@ function initPortpolio() {
   }
 }
 
-// window.addEventListener('load', () => {
+function getProduct() {
+  console.log('getProduct');
+  // alert('clientGet')
+  return new Promise(resolve => {
+    var productsAPI = `${API_PATH}/get/products`;
+    $.getJSON(productsAPI, {
+      format: 'json'
+    }).done(function (response) {
+      let products = '';
+      $.each(response.data, function (i, items) {
+        products += `
+                  <div class="col-lg-3 col-md-4 col-6">
+                      <div class="client-logo">
+                          <img src="${BASE_URL}/uploads/clients/${items.icon}" class="img-fluid" alt="${items.name}" title="${items.description}">
+                      </div>
+                  </div>
+                  <div class="col-lg-2 col-md-6 portfolio-item filter-${str.toLowerCase(items.id_category_product)} shadow-sm" style="position: absolute; left: 760px; top: 230.25px;">
+                      <img src="${BASE_URL}/uploads/products/${items.icons}" class="img-fluid" alt="${items.name}">
+                      <div class="portfolio-info">
+                          <h4>${items.name}</h4>
+                          <p>${items.description}</p>
+                          <-- <a href="#" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a> -->
+                          <a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                      </div>
+                  </div>
+                  `;
+        $('#productData').html(products);
+        resolve(true);
+      });
+    });
 
+  });
+}
 
-//   });
+async function initFetch() {
+  console.log('initFetch');
+  await getClients();
+  usersProduct();
+
+}
