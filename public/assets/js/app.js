4586d0d99cc77d81d211a2d2901b4cc491a69b0d
ibnurizqiaramadan <dadanibnu61@gmail.com>
2021-07-09 16:15:58 +0700

    change to online socket server

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,9 +11,9 @@ var htmlEditor = '', cssEditor = '', jsEditor = '';
 var socket = []
 
 if (typeof io !== 'undefined') {
-	// socket = io.connect(`https://socket.xyrus10.com`)
+	socket = io.connect(`https://socket.xyrus10.com`)
 	// socket = io.connect(`http://192.168.1.69:6996`)
-	socket = io.connect(`http://localhost:6996`)
+	// socket = io.connect(`http://localhost:6996`)
 	// socket = io.connect(`https://ipdn-socket.herokuapp.com`)
 	socket.on("connect", () => {
 		console.log("socket connected")
