f148556ab1cdb3553ccbedc0428d3c0e226b09b2
Ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-01-12 20:19:45 +0700

    change socket

--- public/assets/js/appHome.js
+++ public/assets/js/appHome.js
@@ -5,7 +5,7 @@ const BASE_URL = $('meta[name="baseUrl"]').attr("content"),
 var socket = []
 
 if (typeof io !== 'undefined') {
-  socket = io.connect(`https://socket.xyrus10.com`)
+  socket = io.connect(`https://socket-dgt.inh.pw/`)
   // socket = io.connect(`http://192.168.1.69:6996`)
   // socket = io.connect(`http://localhost:6996`)
   // socket = io.connect(`https://ipdn-socket.herokuapp.com`)
